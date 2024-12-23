d738f1d814ca3b15148ed7cd61082120e741f56e
cgj <cgj@gmail.com>
2024-12-23 08:40:18 +0800

    main first

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,10 @@
+
+/**
+ * @author cgj
+ * @date 12/23/24 8:38 AM
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("master first");
+    }
+}
